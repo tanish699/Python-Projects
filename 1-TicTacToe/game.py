cb8a380bb46965df8739a0b7ef60c0b257ff1bfe
Tanish Wadhwa <144587617+tanish699@users.noreply.github.com>
2026-09-19 18:14:58 +0530

    TicTacToe Game

--- 1-TicTacToe/game.py
+++ 1-TicTacToe/game.py
@@ -1,64 +1,20 @@
-
-# Initial Board print
-def boardRepresentation():
+def playGame() :
     board = [[1,2,3],[4,5,6],[7,8,9]]
+    print("Current is Tic-Tac-Toe Board, Enter the number where you want to mark at your turn. Player 1 will be marked with 'X' and Player 2 will be marked with 'O'")
+    printBoard(board)
+
+    print("Game Starting Now")
+
+
+
+
+def printBoard(board):
     for row in board:
-        print(' | '.join(map(str,row)))
-
-
-
-# Function to print Board
-def displayBoard(board):
-    for element in board:
-        print(' | '.join(map(str,element)))
-
-def playerinput(board,index,mark):
-
-    match
-
-    if board[num] == '-':
-        board[num] = mark
-        return True
-    else:
-        print("this position is already Marked, Please Choose another position")
-        return False
-
-
-def isBoardFull(board):
-    for element in board:
-        if element == '-':
-            return False
-
-    return True
-
-
-def StartGame(board):
-    print("This is the initial Board. At you turn Just give the number as input where you want to mark. Player1 - X, Player2 - O")
-    boardRepresentation()
-
-    if isBoardFull != True:
-        for i in range(1,9):
-            if i%2!=0:
-                print("Player1 Turn")
-                num = int(input())
-                playerinput(board,num,'X')
-                displayBoard(board)
-            else:
-                print("Player2 Turn")
-                num = int(input())
-                playerinput(board,num,'O')
-                displayBoard(board)
-                
-
-
-
-board = [
-    ['-','-','-'],
-    ['-','-','-'],
-    ['-','-','-']
-]
+        print("|".join(map(str,row)))
 
 
 
 
-StartGame(board)
+
+
+playGame()
